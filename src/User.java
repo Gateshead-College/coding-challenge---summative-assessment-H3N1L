683b34442a62c1ca864c2bd747b9231b246b44da
Soni <henil.soni@accenture.com>
2021-12-16 10:44:16 +0000

    User model added

---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+
+    String username;
+    String Password;
+    String forename;
+    String surname;
+    int employeeID;
+
+
+    public User(String username, String password, String forename, String surname, int employeeID) {
+        this.username = username;
+        Password = password;
+        this.forename = forename;
+        this.surname = surname;
+        this.employeeID = employeeID;
+    }
+}
